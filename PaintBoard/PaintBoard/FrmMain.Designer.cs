bef927b01176dfb71447ccb9e6b1210b7358390c
dahyun_0_1 <changum0520@gmail.com>
2025-04-26 22:08:28 +0900

    윈폼 그림판 만들기 - 펜 색상, 지우개, 컨버스 배경색 변경 기능 구현

--- PaintBoard/PaintBoard/FrmMain.Designer.cs
+++ PaintBoard/PaintBoard/FrmMain.Designer.cs
@@ -29,24 +29,90 @@
         private void InitializeComponent()
         {
             WhiteBoard = new PictureBox();
+            DlgColor = new ColorDialog();
+            BtnPenColor = new Button();
+            BtnEraser = new Button();
+            BtnAllClear = new Button();
+            BtnPen = new Button();
+            BtnBackgroundColor = new Button();
             ((System.ComponentModel.ISupportInitialize)WhiteBoard).BeginInit();
             SuspendLayout();
             // 
             // WhiteBoard
             // 
             WhiteBoard.BackColor = Color.White;
-            WhiteBoard.Location = new Point(12, 12);
+            WhiteBoard.Location = new Point(15, 16);
+            WhiteBoard.Margin = new Padding(4);
             WhiteBoard.Name = "WhiteBoard";
-            WhiteBoard.Size = new Size(487, 426);
+            WhiteBoard.Size = new Size(626, 568);
             WhiteBoard.TabIndex = 0;
             WhiteBoard.TabStop = false;
+            WhiteBoard.MouseDown += WhiteBoard_MouseDown;
+            WhiteBoard.MouseMove += WhiteBoard_MouseMove;
+            WhiteBoard.MouseUp += WhiteBoard_MouseUp;
+            // 
+            // BtnPenColor
+            // 
+            BtnPenColor.Location = new Point(648, 51);
+            BtnPenColor.Name = "BtnPenColor";
+            BtnPenColor.Size = new Size(94, 29);
+            BtnPenColor.TabIndex = 1;
+            BtnPenColor.Text = "펜 색상";
+            BtnPenColor.UseVisualStyleBackColor = true;
+            BtnPenColor.Click += BtnColor_Click;
+            // 
+            // BtnEraser
+            // 
+            BtnEraser.Location = new Point(748, 16);
+            BtnEraser.Name = "BtnEraser";
+            BtnEraser.Size = new Size(94, 29);
+            BtnEraser.TabIndex = 2;
+            BtnEraser.Text = "지우개";
+            BtnEraser.UseVisualStyleBackColor = true;
+            BtnEraser.Click += BtnEraser_Click;
+            // 
+            // BtnAllClear
+            // 
+            BtnAllClear.Location = new Point(848, 16);
+            BtnAllClear.Name = "BtnAllClear";
+            BtnAllClear.Size = new Size(103, 29);
+            BtnAllClear.TabIndex = 2;
+            BtnAllClear.Text = "전체 지우개";
+            BtnAllClear.UseVisualStyleBackColor = true;
+            BtnAllClear.Click += BtnAllClear_Click;
+            // 
+            // BtnPen
+            // 
+            BtnPen.Location = new Point(648, 16);
+            BtnPen.Name = "BtnPen";
+            BtnPen.Size = new Size(94, 29);
+            BtnPen.TabIndex = 3;
+            BtnPen.Text = "펜";
+            BtnPen.UseVisualStyleBackColor = true;
+            BtnPen.Click += BtnPen_Click;
+            // 
+            // BtnBackgroundColor
+            // 
+            BtnBackgroundColor.Location = new Point(748, 51);
+            BtnBackgroundColor.Name = "BtnBackgroundColor";
+            BtnBackgroundColor.Size = new Size(94, 29);
+            BtnBackgroundColor.TabIndex = 3;
+            BtnBackgroundColor.Text = "캔버스 색";
+            BtnBackgroundColor.UseVisualStyleBackColor = true;
+            BtnBackgroundColor.Click += BtnBackgroundColor_Click;
             // 
             // FrmMain
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1029, 600);
+            Controls.Add(BtnBackgroundColor);
+            Controls.Add(BtnPen);
+            Controls.Add(BtnAllClear);
+            Controls.Add(BtnEraser);
+            Controls.Add(BtnPenColor);
             Controls.Add(WhiteBoard);
+            Margin = new Padding(4);
             Name = "FrmMain";
             Text = "그림판";
             Load += FrmMain_Load;
@@ -57,5 +123,11 @@
         #endregion
 
         private PictureBox WhiteBoard;
+        private ColorDialog DlgColor;
+        private Button BtnPenColor;
+        private Button BtnEraser;
+        private Button BtnAllClear;
+        private Button BtnPen;
+        private Button BtnBackgroundColor;
     }
 }
--- PaintBoard/PaintBoard/FrmMain.cs
+++ PaintBoard/PaintBoard/FrmMain.cs
@@ -8,8 +8,12 @@ namespace PaintBoard
         }
         Bitmap canvas;
         Graphics g;
-        Pen currentPen = new Pen(Color.Black, 2);
+        Pen drawPen = new Pen(Color.Black, 2);
+        Pen eraserPen = new Pen(Color.White, 2);
+
         bool isDrawing = false;
+        bool isEraseMode = false;
+        Color backgroundColor = Color.White;
         Point prevPoint;
 
 
@@ -20,6 +24,65 @@ namespace PaintBoard
             g = Graphics.FromImage(canvas);
             g.Clear(Color.White);
             WhiteBoard.Image = canvas;
+        }
+
+        private void WhiteBoard_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            prevPoint = e.Location;
+        }
+
+        private void WhiteBoard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                Pen penToUse = isEraseMode ? eraserPen : drawPen;
+                g.DrawLine(penToUse, prevPoint, e.Location);  // 이동한 선 그리기
+                prevPoint = e.Location;     // 현재 좌표를 다음 출발점으로
+                WhiteBoard.Invalidate();
+            }
+        }
+
+        private void WhiteBoard_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void BtnColor_Click(object sender, EventArgs e)
+        {
+            if (DlgColor.ShowDialog() == DialogResult.OK)
+            {
+                drawPen.Color = DlgColor.Color;
+            }
+        }
+
+        private void BtnAllClear_Click(object sender, EventArgs e)
+        {
+            g.Clear(backgroundColor);
+            WhiteBoard.Invalidate();
+
+        }
+
+        private void BtnEraser_Click(object sender, EventArgs e)
+        {
+            isEraseMode = true;
+        }
+
+
+        private void BtnPen_Click(object sender, EventArgs e)
+        {
+            isEraseMode = false;
+        }
+
+        private void BtnBackgroundColor_Click(object sender, EventArgs e)
+        {
+            if (DlgColor.ShowDialog() == DialogResult.OK)
+            {
+                backgroundColor = DlgColor.Color;
+                g.Clear(backgroundColor);           // 배경 색상 변경
+                eraserPen.Color = backgroundColor;  // 지우개 색상 배경색과 동일하게
+                WhiteBoard.Invalidate();            // 화면 갱신
+            }
         }
     }
 }
